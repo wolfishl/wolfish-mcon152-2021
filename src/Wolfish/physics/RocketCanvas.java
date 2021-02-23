eadd5f05d41ca60ea10837e039aa6eac02815e6e
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-02-22 19:41:18 -0500

    The GUI of rocket now also displays the rocket's path

--- src/Wolfish/physics/RocketCanvas.java
+++ src/Wolfish/physics/RocketCanvas.java
@@ -3,6 +3,7 @@ package Wolfish.physics;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 /*
 Draws the rocket flight to the canvas.
@@ -14,21 +15,31 @@ public class RocketCanvas extends Canvas {
     @param rocket
      */
     public void drawRocket(Rocket rocket){
+
         GraphicsContext context = getGraphicsContext2D();
 
         context.setFill(Color.PAPAYAWHIP);
         context.fillRect(0, 0, getWidth(), getHeight());
-
-        context.setFill(Color.AQUAMARINE);
+        context.setFill(Color.DEEPPINK);
         context.translate(0, getHeight());
+        context.setStroke(Color.CORAL);
+        context.moveTo(rocket.getX(0),rocket.getY(0));
+
+
         for (double time = 0; time < rocket.getFlightTime(); time+=0.5)
         {
             double x = rocket.getX(time);
             double y = rocket.getY(time);
 
             context.fillOval(x, -y, 10, 10);
+
+            context.lineTo(x+5, -y);
+            context.stroke();
         }
 
-        context.restore();
+        context.lineTo(rocket.getX(rocket.getFlightTime())+5, -rocket.getY(rocket.getFlightTime()));
+        context.stroke();
+
+        context.translate(0, -getHeight());
     }
 }
