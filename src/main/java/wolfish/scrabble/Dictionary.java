6e20304a8e73e9370283f8b6e855dd080b19520f
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-03-02 20:05:52 -0500

    Added javadoc comments

--- src/main/java/wolfish/scrabble/Dictionary.java
+++ src/main/java/wolfish/scrabble/Dictionary.java
@@ -13,25 +13,29 @@ public class Dictionary {
     private final Map<String, String> wordsToDefinition = new HashMap<>();
 
     /*
-    The constructor opens a file containing valid words and creates an ArrayList of the words
+    The constructor opens a file containing valid words and creates a map of the words and their definitions.
      */
     public Dictionary(String filename) throws FileNotFoundException {
         Scanner inputFile = new Scanner(new File(filename));
 
         while (inputFile.hasNext()) {
             wordsToDefinition.put(inputFile.next(), inputFile.nextLine().trim());
-
         }
 
     }
 
     /*
-    This method takes a String and determines if it is a word
+    This method takes a String and determines if it is a word.
      */
     public boolean determineIfWordIsValid(String word) {
         return wordsToDefinition.containsKey(word.toUpperCase());
     }
 
+
+    /*
+    This method takes a String and returns its definition if it is a valid word.
+    If it is not valid, it returns an empty String.
+     */
     public String getDefinition(String word)
     {
         String definition = wordsToDefinition.get(word.toUpperCase());
