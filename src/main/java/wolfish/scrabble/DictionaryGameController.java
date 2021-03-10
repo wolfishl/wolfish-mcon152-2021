420de783da38bfc63eef23c07b06c128516133be
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-03-09 22:24:12 -0500

    implemented the new scrabble-like game

--- src/main/java/wolfish/scrabble/DictionaryGameController.java
+++ src/main/java/wolfish/scrabble/DictionaryGameController.java
@@ -7,16 +7,111 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class DictionaryGameController {
     @FXML
-    Button tile1;
+    List<Button> tiles;
+    @FXML
+    TextField word;
+    @FXML
+    Label score;
+    @FXML
+    Label invalid;
+
+
+    private LetterBag letterBag = new LetterBag();
+
+    private Dictionary dict;
+
+    public DictionaryGameController() {
+        try {
+            dict = new Dictionary("dictionary.txt");
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void initialize() {
+        for (Button button : tiles) {
+            button.setText(letterBag.nextLetter());
+        }
+    }
+
+    public void onTileClick(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        String letter = button.getText();
+        if (!letter.equals("  ")) {
+            word.setText(word.getText() + letter);
+            button.setText("  ");
+        }
+    }
+
+    public void onClear(ActionEvent event) {
+        String current = word.getText();
+        if (!current.equals("")) {
+            String letter = current.substring(current.length() - 1);
+            resetTiles(letter);
+            word.setText(current.substring(0, current.length() - 1));
+        }
+    }
 
 
 
-    public void searchWord(ActionEvent event) throws FileNotFoundException {
-        Dictionary dict = new Dictionary("dictionary.txt");
+    public void onSubmit(ActionEvent event) {
+        if (dict.determineIfWordIsValid(word.getText()) && word.getText().length() > 1) {
+            int points = 0;
+            switch (word.getText().length()) {
+                case 2:
+                    points = 1;
+                    break;
+                case 3:
+                    points = 3;
+                    break;
+                case 4:
+                    points = 5;
+                    break;
+                case 5:
+                    points = 7;
+                    break;
+                case 6:
+                    points = 11;
+                    break;
+                case 7:
+                    points = 13;
+                    break;
+                default:
+                    points = 0;
+            }
+            String[] words = score.getText().split(" ");
+            int number = Integer.parseInt(words[2]);
+            number += points;
+            score.setText(words[0] + " " + words[1] + " " + number);
+            word.setText("");
+            for (Button tile : tiles) {
+                if (tile.getText().equals("  ") && !letterBag.isEmpty()) {
+                    tile.setText(letterBag.nextLetter());
+                }
+            }
+            invalid.setText("");
+        } else {
+            char[] letters = word.getText().toCharArray();
+            for (char letter : letters) {
+                resetTiles(Character.toString(letter));
+            }
+            word.setText("");
+            invalid.setText("invalid word");
+        }
+    }
 
-
+    public void resetTiles(String letter) {
+        for (Button tile : tiles) {
+            if (tile.getText().equals("  ")) {
+                tile.setText(letter);
+                break;
+            }
+        }
     }
 }
